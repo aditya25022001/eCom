58af25bb4db24bda9ed98425bd66c635e43a7a15
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-02-18 15:41:39 +0530

    cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Message } from '../components/Message'
+import { Link } from 'react-router-dom'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { addToCart } from '../actions/cartActions'
 
-export const CartScreen = () => {
+export const CartScreen = ({ match, location, history }) => {
+
+    const productId = match.params.id
+
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+
+    const { cartItems } = cart
+
+    useEffect(() => {
+        if(productId)
+            dispatch(addToCart(productId, qty))
+    }, [dispatch, productId, qty])
+
     return (
-        <div>
-            Cart
-        </div>
+        <Row>
+            <Col md={8}>
+                <h1>Shopping Cart</h1>
+                {cartItems.length === 0 
+                ? ( 
+                    <Message 
+                        variant="primary">
+                        Your cart in is empty { }  
+                        <Link to="/">
+                            Go Back
+                        </Link> 
+                    </Message>
+
+                ) 
+                : (
+                    <ListGroup variant="flush">
+
+                    </ListGroup>
+                ) }
+            </Col>
+            <Col md={2}>
+
+            </Col>
+            <Col md={2}>
+
+            </Col>
+        </Row>
     )
 }
