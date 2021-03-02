e6253c11f5c1b6fdfd54a3c809b8d011f2b0bb0c
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-03-02 22:01:16 +0530

    frontend auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap';
 import { HomeScreen } from './screens/HomeScreen';
 import { ProductScreen } from './screens/ProductScreen';
 import { CartScreen } from './screens/CartScreen';
+import { LoginScreen } from './screens/LoginScreen';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
@@ -12,9 +13,10 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path="/" component={HomeScreen} exact />   {/* component says whichone to open when / achieved and exact syas that excact path is needed */}
+          <Route path='/login' component={LoginScreen}></Route>
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} /> {/* ? in any path says that the passed parameter in optional */}
+          <Route path="/" component={HomeScreen} exact />   {/* component says whichone to open when / achieved and exact syas that excact path is needed */}
         </Container>
       </main>
       <Footer />
