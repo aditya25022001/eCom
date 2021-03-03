9e29da5b27468d6531327cfd6eb445a5ba78f427
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-03-03 13:26:51 +0530

    added year of copyright

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 
 export const Footer = () => {
+
+    const date = new Date();
+
     return (
         <footer>
             <Container>
             <Row>
-                <Col className='text-center py-3'>Copyright {String.fromCharCode(169)}</Col>
+                <Col className='text-center py-3'>Copyright {String.fromCharCode(169)} {date.getFullYear()} </Col>
             </Row>
             </Container>
         </footer>
